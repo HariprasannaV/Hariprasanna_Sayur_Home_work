6ae54a3070e4c87436a5ed1b1a908ed5bcebee19
HariprasannaV <hhaarrii2328@gmail.com>
2023-10-04 19:37:50 +0530

    .

--- Logical Thinking/function1/problem 5.py
+++ Logical Thinking/function1/problem 5.py
@@ -3,12 +3,13 @@ Problem 5:
 Write a program to sort an array of numbers in ascending order. Use functions.
 """
 
+
 def sorting():
     integer_array = []
-    for i in (input("Enter the Array of Numbers here  : ")).split():
+    num_list = input("Enter the Array of Numbers here  : ").split()
+    for i in num_list:
         integer_array.append(int(i))
     print(sorted(integer_array))
-
 
 
 sorting()
