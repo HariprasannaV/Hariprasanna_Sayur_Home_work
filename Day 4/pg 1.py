042d03b99deedeba4d18f71c1b430868fb0dd57f
HariprasannaV <hhaarrii2328@gmail.com>
2023-11-30 16:55:38 +0530

    .

--- Day 4/pg 1.py
+++ Day 4/pg 1.py
@@ -5,17 +5,26 @@ letters in each word while keeping the word order intact. Use function.
 Eg- input - I am Sayur
 Output I ma ruyaS
 """
-import re
+# import re
+#
+# # Get input sentence from the user
+# ip_sentence = str(input("Enter the Sentence:\n"))
+#
+# # Use regular expression to find all words in the sentence
+# words = re.findall(r'\b\w+\b', ip_sentence)
+#
+# # Reverse each word and print them
+# for word in words:
+#     # Reverse the word using slicing and print with a space at the end
+#     print(word[::-1], end=" ")
 
 # Get input sentence from the user
-ip_sentence = str(input("Enter the Sentence:\n"))
+sentence = str(input("Enter the Sentence : \n"))
 
-# Use regular expression to find all words in the sentence
-words = re.findall(r'\b\w+\b', ip_sentence)
+words = sentence.split() # splitting the sentence
+
 
 # Reverse each word and print them
 for word in words:
     # Reverse the word using slicing and print with a space at the end
     print(word[::-1], end=" ")
-
-# End of the program
